6a944ef8bd45213deefee9e5771953bce65f936f
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-20 18:21:01 +0700

    add warning to kasus endpoint

--- api/kasus/index.ts
+++ api/kasus/index.ts
@@ -4,6 +4,7 @@ import { fetchAllKasus } from "../../util/fetcher";
 
 export default async (_, response: NowResponse) => {
   response.json({
+    warning: `The data source for this endpoint is no longer being maintained. See https://twitter.com/salmayarista/status/1240959718580826113`,
     data: await fetchAllKasus()
   });
 };
