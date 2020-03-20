bfa55f2f0c1363f41bc1438a16067ed883801efe
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-20 14:49:02 +0700

    fix provinsi query

--- api/provinsi/index.ts
+++ api/provinsi/index.ts
@@ -8,19 +8,15 @@ import { endpoints } from "../../util/endpoints";
 import { createArrayQuery, where } from "../../util/query";
 
 const getProvinsiData = async (
-  orderByFields = "Kasus_Terkonfirmasi_Kumulatif desc"
+  orderByFields = "Kasus_Terkonfirmasi_Akumulatif desc"
 ) => {
-  const features = await fetchFeatures(
+  return fetchFeatures(
     `https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query`,
     createArrayQuery({
       where: where.all,
       orderByFields
     })
   );
-
-  const data = features.map(attributeSpreader).map(normalizeKeys);
-
-  return data;
 };
 
 export default async (_, response: NowResponse) => {
