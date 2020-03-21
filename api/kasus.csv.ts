306229a8857d9c4a70e33f3c7f1d4b3b6b39257f
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-21 12:46:24 +0700

    fix kasus csv

--- api/kasus.csv.ts
+++ api/kasus.csv.ts
@@ -1,11 +1,11 @@
 import { NowResponse } from "@now/node";
 import ObjectsToCsv from "objects-to-csv";
 
-import { fetchAllKasus } from "../util/fetcher";
+import { fetchCaseGraph } from "../util/fetcher";
 
 export default async (_, response: NowResponse) => {
-  const data = await fetchAllKasus();
-  const csv = new ObjectsToCsv(data);
+  const { nodes } = await fetchCaseGraph();
+  const csv = new ObjectsToCsv(nodes);
   const csvString = await csv.toString();
   response.setHeader("Content-Type", "text/csv");
   response.send(csvString);
