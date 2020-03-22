643c161b295e3a56dc656ee15a74eef8c213cced
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-22 15:23:15 +0700

    change front margin

--- util/template.ts
+++ util/template.ts
@@ -14,6 +14,7 @@ function getCss(width: number, height: number, imagesLength = 450) {
   const marginSize = Math.floor(areaRoot / 8);
   const safetyMargin = 2;
   const imageWidth = areaRoot - 2 * marginSize - safetyMargin;
+  const frontMargin = Math.floor(imageWidth * 0.4);
   return `
   @font-face {
     font-family: 'Inter';
@@ -72,9 +73,9 @@ body {
 
 .front {
   z-index: 2;
-  margin-top: 8px;
-  margin-left: 8px;
-  width: ${imageWidth - 8}px;
+  margin-top: ${frontMargin}px;
+  margin-left: ${frontMargin}px;
+  width: ${imageWidth - frontMargin}px;
 }
 
   `;
