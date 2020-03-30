4adcb854021c573e551a00ca7f70d657b5deebb1
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-30 18:06:41 +0700

    wisma atlet no recap

--- api/wisma-atlet/karyawan.ts
+++ api/wisma-atlet/karyawan.ts
@@ -5,27 +5,23 @@ import { NowResponse } from "@now/node";
 const fetch = withRetry(unfetch);
 
 export const fetchWismaAtletRuangan = async () => {
-  return (
-    await fetch(`https://u071.zicare.id/masterdata/getRuangRawat`, {
-      method: "POST",
-      body: JSON.stringify({}),
-      headers: {
-        "x-requested-with": "XMLHttpRequest"
-      }
-    }).then(res => res.json())
-  ).recap[0];
+  return await fetch(`https://u071.zicare.id/masterdata/getRuangRawat`, {
+    method: "POST",
+    body: JSON.stringify({}),
+    headers: {
+      "x-requested-with": "XMLHttpRequest"
+    }
+  }).then(res => res.json());
 };
 
 export const fetchWismaAtletKaryawan = async () => {
-  return (
-    await fetch(`https://u071.zicare.id/masterdata/getKaryawan`, {
-      method: "POST",
-      body: JSON.stringify({}),
-      headers: {
-        "x-requested-with": "XMLHttpRequest"
-      }
-    }).then(res => res.json())
-  ).recap[0];
+  return await fetch(`https://u071.zicare.id/masterdata/getKaryawan`, {
+    method: "POST",
+    body: JSON.stringify({}),
+    headers: {
+      "x-requested-with": "XMLHttpRequest"
+    }
+  }).then(res => res.json());
 };
 
 export default async (_, response: NowResponse) => {
