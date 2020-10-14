7b6136c46c60a1f199a6a11b13456aae38c78f56
Yogs <yogazacty@gmail.com>
2020-10-14 15:34:10 +0700

    update arcgis endpoint

--- util/endpoints.ts
+++ util/endpoints.ts
@@ -1,5 +1,6 @@
 export const endpoints = {
-  statistikPerkembangan: `https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/Statistik_Perkembangan_COVID19_Indonesia/FeatureServer/0/query`,
+  statistikPerkembanganOld: `https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/Statistik_Perkembangan_COVID19_Indonesia/FeatureServer/0/query`,
+  statistikPerkembangan: `https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/ArcGIS/rest/services/Statistik_Perkembangan_COVID19_Indonesia_New/FeatureServer/0/query`,
   perProvinsi: `https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query`,
   kasusCOVIDGsheet: `https://services8.arcgis.com/mpSDBlkEzjS62WgX/arcgis/rest/services/Kasus_COVID19_Indonesia_gsheet/FeatureServer/0/query`
 };
